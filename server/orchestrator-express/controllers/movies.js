1f34c485e6ec195ccefa01c2a911348dba5997db
Yudhistira Tribuana Authar <yudhistira.tribuana@gmail.com>
2023-06-22 13:57:02 +0700

    Added redis caching to movie fetch endpoint

--- server/orchestrator-express/controllers/movies.js
+++ server/orchestrator-express/controllers/movies.js
@@ -4,7 +4,13 @@ const MOVIE_URL = process.env.MOVIE_URL || "http://localhost:3000";
 module.exports = class MovieController {
 	static async fetchMovies(req, res, next) {
 		try {
+			const movie = await redis.get("movies");
+			if (movie) {
+				res.status(200).send(JSON.parse(movie));
+				return;
+			}
 			const { data } = await axios.get(MOVIE_URL + "/movies");
+			redis.set("movies", JSON.stringify(data));
 			res.status(200).send(data);
 		} catch (error) {
 			next(error);
