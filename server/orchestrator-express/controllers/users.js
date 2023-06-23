b553010c0eafc5d63f659f74604da37be0507089
Yudhistira Tribuana Authar <yudhistira.tribuana@gmail.com>
2023-06-23 16:29:54 +0700

    Added users fetch all and user fetch by detail endpoints

--- server/orchestrator-express/controllers/users.js
+++ server/orchestrator-express/controllers/users.js
@@ -2,3 +2,50 @@ const axios = require("axios");
 const redis = require("../config/redisConnection");
 const MOVIE_URL = process.env.MOVIE_URL || "http://localhost:3000";
 const USER_URL = process.env.USER_URL || "http://localhost:3001";
+
+module.exports = class UserController {
+	static async findAllUsers(req, res, next) {
+		try {
+            const usersCache = await redis.get("users");
+            if (usersCache) {
+                res.status(200).json(JSON.parse(usersCache));
+                return;
+            }
+            const { data:users } = await axios.get(USER_URL + "/users");
+            redis.set("users", JSON.stringify(users.data));
+			res.status(200).json(users.data);
+		} catch (error) {
+			next(error);
+		}
+	}
+	static async findUserById(req, res, next) {
+		try {
+            const { id } = req.params;
+            const userCache = await redis.get(`user:${id}`);
+            if (userCache) {
+                res.status(200).json(JSON.parse(userCache));
+                return;
+            }
+            const { data:user } = await axios.get(USER_URL + "/users/" + id);
+            redis.set(`user:${id}`, JSON.stringify(user.data));
+			res.status(200).json(user.data);
+			res.status(501).send("under construction");
+		} catch (error) {
+			next(error);
+		}
+	}
+	static async createUser(req, res, next) {
+		try {
+			res.status(501).send("under construction");
+		} catch (error) {
+			next(error);
+		}
+	}
+	static async deleteUserById(req, res, next) {
+		try {
+			res.status(501).send("under construction");
+		} catch (error) {
+			next(error);
+		}
+	}
+};
--- server/orchestrator-express/routes/index.js
+++ server/orchestrator-express/routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const MovieController = require("../controllers/movies");
+const UserController = require("../controllers/users");
 const router = express.Router();
 
 router.get("/movies", MovieController.fetchMovies);
@@ -7,4 +8,9 @@ router.post("/movies", MovieController.createMovie);
 router.delete("/movies/:id", MovieController.deleteMovie);
 router.put("/movies/:id", MovieController.editMovie);
 router.get("/movies/:id", MovieController.fetchMovieDetail);
+
+router.get("/users/", UserController.findAllUsers);
+router.get("/users/:id", UserController.findUserById);
+router.post("/users/", UserController.createUser);
+router.delete("/users/:id", UserController.deleteUserById);
 module.exports = router;
